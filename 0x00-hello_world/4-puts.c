8340791439a9d04513ab487711955713933993f5
“ealwaafy” <ealwaafy@gmail.com>
2022-07-01 11:39:06 -0700

    cool

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
  * * main - prints the size of various types
  * * Return: 0 if exited properly, non-zero otherwise
  * */
-int main(void) {
-	puts("\"programming is kk");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
