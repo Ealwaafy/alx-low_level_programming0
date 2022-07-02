56d44f26c3b4205e4908a1a94c15ad19f5b90e31
“ealwaafy” <ealwaafy@gmail.com>
2022-07-02 02:49:45 -0700

    col

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <stdio.h>
 /**
  * * main - prints the size of various types
  * * Return: 0 if exited properly, non-zero otherwise
  * */
 int main(void)
 {                                                                                                                                                                               
-puts("\"Programing is like building multilingual puzzle")                                                                                                                             
-		                return(0);
+puts("\"Programing is like building multilingual puzzle");
+return (0);
 }
