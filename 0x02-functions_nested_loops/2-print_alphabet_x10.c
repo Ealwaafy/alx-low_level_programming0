fee460c225646e01c29997b95ab698f9405b561d
“ealwaafy” <ealwaafy@gmail.com>
2022-07-05 05:28:50 -0700

    co

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,23 @@
-#include "main.h"
-/**
- * print_alphabet_x10 - print the alphabet 10 times.
- *
- * Return: Always 0 (Success)
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Brennan D Baraban
  */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+
 void print_alphabet_x10(void)
-{int alpha;
-	int count;
-	count = 0;
-	while (count <= 9)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ <= 9)
 	{
-		alpha = 'a';
-		while (alpha <= 'z')
-		{
-			_putchar(alpha);
-			alpha++;
-		}
-		count++;
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
