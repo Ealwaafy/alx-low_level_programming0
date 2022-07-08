c44a91e223b14d1d291b19699ab7e8f750be73bb
“ealwaafy” <ealwaafy@gmail.com>
2022-07-08 01:31:29 -0700

    fixing issues

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
-#ifndef main_h
-#define main_h
+
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-#endif /* main_h */
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
