0b5e78193c0dc2e347fb609e086277e9b8002485
“ealwaafy” <ealwaafy@gmail.com>
2022-07-14 05:55:01 -0700

    cool

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,33 +1,26 @@
 #include "main.h"
 
 /**
- * _strspn - Gets the length of a prefix substring.
- * @s: The string to be searched.
- * @accept: The prefix to be measured.
+ * _strcmp - function with two argument
+ * @s1: pointer char type 1st argument
+ * @s2: pointer char type 2nd argument
  *
- * Return: The number of bytes in s which
- *         consist only of bytes from accept.
+ * Description: compare two strings
+ * Return: 0 for same string, other for not same
  */
-unsigned int _strspn(char *s, char *accept)
+int _strcmp(char *s1, char *s2)
 {
-	unsigned int bytes = 0;
-	int index;
-
-	while (*s)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		for (index = 0; accept[index]; index++)
-		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
-
-			else if (accept[index + 1] == '\0')
-				return (bytes);
-		}
-
-		s++;
+		s1++;
+		s2++;
 	}
-	return (bytes);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
